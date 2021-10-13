000c90cc6b82c5d1569467196244806d3246710b
Ganesh V. Pardeshi <pardeshivg002@gmail.com>
2021-10-13 10:50:21 +0530

    code

--- Data Structures in C/Bubble_sort_1.c
+++ Data Structures in C/Bubble_sort_1.c
@@ -5,10 +5,10 @@ int main()
     int a[100];
     int n=0,temp=0;
 
-    printf("enter the number ");
+    printf("Enter the number ");
     scanf("%d",&n);
 
-    printf("Enter the %d elements : \n",n);
+    printf("Enter the %d Elements : \n",n);
     for(i = 0; i < n; i++)
     {
          scanf("%d",&a[i]);
@@ -20,13 +20,9 @@ int main()
         {
             if(a[j]>a[j+1])
             {
-                //swaping
-                // temp    =  a[i];
-                // a[i]    =  a[j];
-                // a[j]  = temp;
-                temp = a[j];
-                a[j] =a[j+1];
-                a[j+1] = temp;
+                temp   =  a[j];
+                a[j]   =  a[j+1];
+                a[j+1] =  temp;
             }
         }
     }
@@ -37,39 +33,3 @@ int main()
     }
     return 0;
 }
-
-
-
-// #include <stdio.h>
-// int main()
-// {
-//   int array[100], n, c, d, swap;
-
-//   printf("Enter number of elements\n");
-//   scanf("%d", &n);
-
-//   printf("Enter %d integers\n", n);
-
-//   for (c = 0; c < n; c++)
-//     scanf("%d", &array[c]);
-
-//   for (c = 0 ; c < n - 1; c++)
-//   {
-//     for (d = 0 ; d < n - c - 1; d++)
-//     {
-//       if (array[d] > array[d+1]) /* For decreasing order use '<' instead of '>' */
-//       {
-//         swap       = array[d];
-//         array[d]   = array[d+1];
-//         array[d+1] = swap;
-//       }
-//     }
-//   }
-
-//   printf("Sorted list in ascending order:\n");
-
-//   for (c = 0; c < n; c++)
-//      printf("%d\n", array[c]);
-
-//   return 0;
-// }
---
+++ Data Structures in C/demo.c
@@ -0,0 +1,66 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int i,j;
+    int a[7]={21 , 3, 5, 12, 11, 17, 26}; //Given Array n=7
+    int n=7,temp=0;
+
+    printf("The Unsotred Array is : \n");
+    for(i=0;i<=n;i++) //printing the given unsorted array
+    {
+        printf("%d ",a[i]);
+    }
+
+    for(i = 0; i < n - 1; i++)   //sorting the array 
+    {
+        for(j = 0; j < n - i - 1; j++)
+        {
+            if(a[j]>a[j+1])
+            {
+                temp   =  a[j];
+                a[j]   =  a[j+1];
+                a[j+1] =  temp;
+            }
+        }
+    }
+    printf("\nSorted list in ascending order:\n");
+    for(i = 0; i < n; i++) //printing the given sorted array
+    {
+        printf("%d ",a[i]);
+    }
+
+    getch();
+    return 0;
+}
+{
+    int i,j;
+    int a[7]={21 , 3, 5, 12, 11, 17, 26};
+    int n=7,temp=0;
+
+    printf("The Unsotred Array is : \n");
+    for(i=0;i<=n;i++)
+    {
+        printf("%d ",a[i]);
+    }
+
+    for(i = 0; i < n - 1; i++)
+    {
+        for(j = 0; j < n - i - 1; j++)
+        {
+            if(a[j]>a[j+1])
+            {
+                temp   =  a[j];
+                a[j]   =  a[j+1];
+                a[j+1] =  temp;
+            }
+        }
+    }
+    printf("\nSorted list in ascending order:\n");
+    for(i = 0; i < n; i++)
+    {
+        printf("%d ",a[i]);
+    }
+    return 0;
+    getch();
+}
